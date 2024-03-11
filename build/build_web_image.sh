f6a58c782058622d2e8805d90044807a5e76e1b6
The Poor Swiss <admin@thepoorswiss.com>
2024-03-11 07:58:29 +0100

    Update scripts

--- build/build_web_image.sh
+++ build/build_web_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-make clean
+docker run -v $(pwd):/home/swr_calculator -w /home/swr_calculator -t swr:build make clean
 docker run -v $(pwd):/home/swr_calculator -w /home/swr_calculator -t swr:build make -j9 release_debug
 docker build -f build/Dockerfile.web -t swr:web .
 docker tag swr:web wichtounet/swr:web
