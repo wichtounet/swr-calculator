d7847a9424e47d9875fb5a6c56c9eba505f05320
Joe Kline <jdomkline@gmail.com>
2024-04-30 11:29:22 -0400

    worker: flip group/facet and add command line arg option

--- scripts/balancing_analysis.py
+++ scripts/balancing_analysis.py
@@ -2,17 +2,43 @@ import requests
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import sys
 
 def print_full(x):
     pd.set_option('display.max_rows', len(x))
     print(x)
     pd.reset_option('display.max_rows')
 
-# Define the URL
-baseurl = "http://localhost:8085/api/simple?inflation=us_inflation&initial=1000&years=30&wr=4.0&end=2022"
+#Default server and port if needed
+server = "localhost"
+port = "8085"
+inflation="us_inflation"
+initial = "1000"
+years = "30"
+wr="4.0"
+start = "1960"
+end = "2020"
+step = "5"
 
-#grab some years that give a full 30 range
-years = range(1970, 2020, 3)
+if len(sys.argv) >=10:
+    server = sys.argv[1]
+    port = sys.argv[2]
+    inflation = sys.argv[3]
+    initial = sys.argv[4]
+    years = sys.argv[5]
+    wr = sys.argv[6]
+    start = sys.argv[7]
+    end = sys.argv[8]
+    step = sys.argv[9]    
+
+else:
+    print("args invalid using default >> balancing_analysis.py localhost 8085 us_inflation 1000 30 4.0 1960 2020 5")    
+
+# Define the URL
+baseurl = "http://"+server+":"+port+"/api/simple?inflation="+inflation+"&initial="+initial+"&years="+years+"&wr="+wr+"&end="+end
+
+#based on the inputs with step years
+years = range(int(start), int(end), int(step))
 portfolios = ["us_stocks:80;us_bonds:20;", "us_stocks:50;us_bonds:50;", "us_stocks:20;us_bonds:80;"]
 balanced= ["none", "monthly", "yearly"]
 
@@ -42,9 +68,9 @@ df = pd.DataFrame(results)
 
 # Create line plot that groups on portfolio and facets on rebalance strategy
 sns.set(style="whitegrid")
-g = sns.FacetGrid(df, col="balanced", col_wrap=3, height=4)
-g.map_dataframe(sns.lineplot, "year", "tv_median", hue="portfolio", marker="o")
-g.set_axis_labels("Year", "TV Median")
+g = sns.FacetGrid(df, col="portfolio", col_wrap=3, height=4)
+g.map_dataframe(sns.lineplot, "year", "tv_median", hue="balanced", marker="o")
+g.set_axis_labels("Year", "Median Term Val")
 g.add_legend()
 plt.show()
 
