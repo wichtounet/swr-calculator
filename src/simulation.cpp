a5c9ba47d72fa42229eb50cbd092ffc1ad2d9441
The Poor Swiss <admin@thepoorswiss.com>
2024-08-26 20:52:17 +0200

    Update files

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,3 +1,10 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2019-2024.
+// Distributed under the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
 #include "simulation.hpp"
 
 #include <algorithm>
