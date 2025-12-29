d51c401c05b248996b8378dc0c73ffbd26b706fa
The Poor Swiss <admin@thepoorswiss.com>
2025-12-29 08:14:15 +0100

    Use the proper image in clean.sh

--- build/clean.sh
+++ build/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v $(pwd):/home/swr_calculator -w /home/swr_calculator -t wichtounet/cpp make clean
+docker run -v $(pwd):/home/swr_calculator -w /home/swr_calculator -t wichtounet/cpp:alpine-latest make clean
